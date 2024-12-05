3b327fc38d0e33d47dcd41f18e00822ced23181c
Elmer Salazar <huntergps79@yahoo.es>
2024-12-04 22:12:19 -0500

    para instalar POS

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
 
-from . import l10n_ec_pos_stock
+from . import pos_stock
 from . import stock_picking
 from . import product_product
 from . import product_template
-from . import bi_pos_session
+from . import pos_session
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
